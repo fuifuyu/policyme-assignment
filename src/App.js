3c92c3022424a7b92d5fe28fbd17add1fe9bccb9
Fuifuyu <32946204+Fuifuyu@users.noreply.github.com>
2024-10-01 14:32:54 -0400

    req 6: Get/Post request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
 import AttributeControl, { convertModifier } from './AttributeControl.js';
@@ -80,65 +80,93 @@ function satisfyClassReq(character, classType) {
   return true;
 }
 
+function saveCharacter(character) {
+  console.log(character)
+  fetch("https://recruiting.verylongdomaintotestwith.ca/api/fuifuyu/character", {
+    method: "POST",
+    body: JSON.stringify(character),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  });
+}
+
 function App() {
-  const [character, setCharacter] = useState(newCharacter());
+  const [character, setCharacter] = useState(null);
   const [showClass, setShowClass] = useState(null);
+
+  useEffect(()=>{
+    fetch("https://recruiting.verylongdomaintotestwith.ca/api/fuifuyu/character", {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(res => res.json()).then(res => {
+      if (res.body !== undefined) setCharacter(res.body);
+      else setCharacter(newCharacter());
+    })
+  }, [])
   
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
-      <section className="App-section">
-        <div className='character'>
-          <div className='card'>
-            <h2>Attributes</h2>
+      {character === null ?
+        <section className='App-section'>
+          Loading...
+        </section> :
+        <section className="App-section">
+          <button onClick={()=>saveCharacter(character)}>Save</button>
+          <div className='character'>
+            <div className='card'>
+              <h2>Attributes</h2>
+              {
+                ATTRIBUTE_LIST.map((attr) => {
+                  return <AttributeControl
+                    name={attr}
+                    key={attr}
+                    attribute={character.attributes[attr]}
+                    onIncrement={()=> setCharacter(incrementAttribute(character, attr))}
+                    onDecrement={()=> setCharacter(decrementAttribute(character, attr))}
+                  />
+                })
+              }
+            </div>
+            <div className='card'>
+              <h2>Classes</h2>
             {
-              ATTRIBUTE_LIST.map((attr) => {
-                return <AttributeControl
-                  name={attr}
-                  key={attr}
-                  attribute={character.attributes[attr]}
-                  onIncrement={()=> setCharacter(incrementAttribute(character, attr))}
-                  onDecrement={()=> setCharacter(decrementAttribute(character, attr))}
+              Object.keys(CLASS_LIST).map((classType) => {
+                return <div key={classType} onClick={()=>setShowClass(classType)} className={satisfyClassReq(character, classType) ? "" : "red"}>{classType}</div>
+              })
+            }
+            </div>
+            {showClass &&
+            <div className='card'>
+              <h2>{showClass} Minimum Requirement</h2>
+              {
+                Object.keys(CLASS_LIST[showClass]).map((attr) => {
+                  return <div>{attr}: {CLASS_LIST[showClass][attr]}</div>;
+                })
+              }
+              <button onClick={()=>setShowClass(null)}>Close requirement window</button>
+            </div>
+            }
+            <div className='card'>
+              <h2>Skills</h2>
+              {SKILL_LIST.map((skill) => {
+                const name = skill.name;
+                const attrName = skill.attributeModifier;
+                return <SkillControl
+                  skill={{name: name, val: character.skills[name]}}
+                  modifier={{name: attrName, val: character.attributes[attrName].modifier}}
+                  onIncrement={() => setCharacter(incrementSkill(character, name))}
+                  onDecrement={() => setCharacter(decrementSkill(character, name))}
                 />
-              })
-            }
+              })}
+            </div>
           </div>
-          <div className='card'>
-            <h2>Classes</h2>
-          {
-            Object.keys(CLASS_LIST).map((classType) => {
-              return <div key={classType} onClick={()=>setShowClass(classType)} className={satisfyClassReq(character, classType) ? "" : "red"}>{classType}</div>
-            })
-          }
-          </div>
-          {showClass &&
-          <div className='card'>
-            <h2>{showClass} Minimum Requirement</h2>
-            {
-              Object.keys(CLASS_LIST[showClass]).map((attr) => {
-                return <div>{attr}: {CLASS_LIST[showClass][attr]}</div>;
-              })
-            }
-            <button onClick={()=>setShowClass(null)}>Close requirement window</button>
-          </div>
-          }
-          <div className='card'>
-            <h2>Skills</h2>
-            {SKILL_LIST.map((skill) => {
-              const name = skill.name;
-              const attrName = skill.attributeModifier;
-              return <SkillControl
-                skill={{name: name, val: character.skills[name]}}
-                modifier={{name: attrName, val: character.attributes[attrName].modifier}}
-                onIncrement={() => setCharacter(incrementSkill(character, name))}
-                onDecrement={() => setCharacter(decrementSkill(character, name))}
-              />
-            })}
-          </div>
-        </div>
-      </section>
+        </section>
+      }
     </div>
   );
 }
